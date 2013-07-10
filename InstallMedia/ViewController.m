e3a2828cdb841c4c1af92ecc0ecbd405b4b2f80d
passya <passya76@gmail.com>
2013-07-10 11:31:34 +0900

    first version

--- InstallMedia/ViewController.m
+++ InstallMedia/ViewController.m
@@ -8,7 +8,12 @@
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController () {
+    NSFileManager* _fm;
+    NSString* _findPath;
+    NSDirectoryEnumerator* _enumrator;
+    NSString* _saveFilePath;
+}
 
 @end
 
@@ -18,7 +23,54 @@
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    _fm = [[NSFileManager alloc] init];
+    _findPath = @"/Users/jspark/Pictures";
+    _enumrator = [_fm enumeratorAtPath:_findPath];
+    [self nextSaveMedia];
 }
+
+
+- (void)nextSaveMedia {
+    while ( nil != (_saveFilePath = [_enumrator nextObject]) ) {
+        NSString* fileExtension = [[_saveFilePath pathExtension] lowercaseString];
+        if ([fileExtension isEqualToString:@"jpg"] ||
+            [fileExtension isEqualToString:@"png"]) {
+            UIImage* image = [UIImage imageWithContentsOfFile:[_findPath stringByAppendingPathComponent:_saveFilePath]];
+            UIImageWriteToSavedPhotosAlbum(image, self, @selector(image:didFinishSavingWithError:contextInfo:), nil);
+            return;
+        }
+        else if ([fileExtension isEqualToString:@"mov"]) {
+            UISaveVideoAtPathToSavedPhotosAlbum([_findPath stringByAppendingPathComponent:_saveFilePath], self, @selector(video:didFinishSavingWithError:contextInfo:), nil);
+            return;
+        }
+    }
+    NSLog(@"Finished");
+}
+
+
+- (void)image:(UIImage *)image didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    [self didFinishSavingWithError:error];
+}
+
+
+
+- (void)video:(NSString *)videoPath didFinishSavingWithError:(NSError *)error contextInfo:(void *)contextInfo {
+    [self didFinishSavingWithError:error];
+}
+
+
+- (void)didFinishSavingWithError:(NSError*)error {
+    if (error != nil) {
+        NSLog(@"Failed: %@, %@", _saveFilePath, error);
+    }
+    else {
+        NSLog(@"Saved: %@", _saveFilePath);
+    }
+    [self nextSaveMedia];
+}
+
+
 
 - (void)didReceiveMemoryWarning
 {
